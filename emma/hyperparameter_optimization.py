d6abcdd4adf829ad94f016b931513d657d04f17a
emmachickles <53268378+emmachickles@users.noreply.github.com>
2020-04-16 19:43:44 -1000

    Update hyperparameter_optimization.py

--- emma/hyperparameter_optimization.py
+++ emma/hyperparameter_optimization.py
@@ -13,15 +13,19 @@ from itertools import product
 from sklearn.metrics import confusion_matrix
 
 
-output_dir = './plots/plots041520-supervised-2/'
+output_dir = './plots/plots041620-1/'
 
 # fname_time = './section1-time.txt'
 # fname_intensity = './section1-intensity.csv'
 # fname_ticid = './section1-ticid.txt'
-fname_time = './supervised100-time.txt'
-fname_intensity = './supervised100-intensity.csv'
-fname_class = './supervised100-classification.txt'
-fname_ticid = './supervised100-ticid.txt'
+# fname_time = './supervised100-time.txt'
+# fname_intensity = './supervised100-intensity.csv'
+# fname_class = './supervised100-classification.txt'
+# fname_ticid = './supervised100-ticid.txt'
+fname_time = '3class-labelled-time.txt'
+fname_intensity = '3class-labelled-intensity.csv'
+fname_class = '3class-labelled-classification.txt'
+fname_ticid = '3class-labelled-ticid.txt'
 cutoff = 8896 # !! get rid of this
 
 # >> lc index in x_test
@@ -31,38 +35,37 @@ inds = [0,1,2,3,4,5,6,7,-1,-2,-3,-4,-5,-6,-7] # >> input_output_plot
 # inds = [0,1,2,3,4,5,6,7,8,9,0,1,2,3,4]
 
 # >> parameters
-p = {'kernel_size': [3],
-      'latent_dim': [15],
-      'strides': [1],
-      'epochs': [50],
-      'dropout': [0.0],
-      'num_conv_layers': [5,7,9,11,13],
-      'num_filters': [[8,8,8,8,8],[8,8,8,8,8,8,8],
-                      [8,8,8,8,8,8,8,8,8],[8,8,8,8,8,8,8,8,8,8,8],
-                      [8,8,8,8,8,8,8,8,8,8,8,8,8]],
-      'batch_size': [32],
-      'activation': ['relu'],
-      'optimizer': ['adam'],
-      'last_activation': ['relu'],
-      'losses': ['mean_squared_error'],
-      'lr': [0.001]}
+# p = {'kernel_size': [3],
+#       'latent_dim': [25],
+#       'strides': [1],
+#       'epochs': [10],
+#       'dropout': [0.5],
+#       'num_conv_layers': [7],
+#       'num_filters': [[32,32,32,32,32,32,32]],
+#       'batch_size': [32],
+#       'activation': ['relu'],
+#       'optimizer': ['adam'],
+#       'last_activation': ['relu'],
+#       'losses': ['mean_squared_error'],
+#       'lr': [0.001]}
 
 # >> supervised
 p = {'kernel_size': [3],
-      'latent_dim': [25],
+      'latent_dim': [15, 25],
       'strides': [1],
-      'epochs': [100],
+      'epochs': [50],
       'dropout': [0.5],
-      'num_conv_layers': [9],
-      'num_filters': [[32,32,64,64,128,128,256,256,512]],
-      'batch_size': [32, 5],
+      'num_conv_layers': [7,9],
+      'num_filters': [[64,64,64,64,64,64,64],
+                      [64,64,64,64,64,64,64,64,64]],
+      'batch_size': [16],
       'activation': ['relu'],
       'optimizer': ['adam'],
       'last_activation': ['relu'],
       'losses': ['categorical_crossentropy'],
       'lr': [0.001]}
 
-# >> random search
+# # >> random search
 # p = {'kernel_size': [3],
 #       'latent_dim': [10,15,20,25,30],
 #       'strides' : [1],
@@ -81,14 +84,14 @@ grid_search = True
 randomized_search = False
 n_iter = 200 # >> for randomized_search
 
-dual_input = False
+dual_input = True
 
 plot_epoch         = True
 plot_in_out        = False
 plot_in_bottle_out = False
 plot_kernel        = False
 plot_intermed_act  = False
-plot_latent_test   = False
+plot_latent_test   = True
 plot_latent_train  = True
 plot_clustering    = False
 make_movie         = False
@@ -99,33 +102,39 @@ addend = 0.
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 # -- x_train, x_test ----------------------------------------------------------
+if supervised:
+    classes = np.loadtxt(fname_class)
+    num_classes = len(np.unique(classes))
+else:
+    classes=False
+
 x = np.loadtxt(fname_time)
 x = np.delete(x, np.arange(cutoff, np.shape(x)[0]), 0)
-x_train, x_test = ml.split_data(fname_intensity, cutoff=cutoff,
-                                train_test_ratio=0.90)
+x_train, x_test, y_train, y_test = ml.split_data(fname_intensity,
+                                                 cutoff=cutoff,
+                                                 train_test_ratio=0.90,
+                                                 supervised=supervised,
+                                                 classes=classes)
 
 ticid = np.loadtxt(fname_ticid)
 ticid_train = ticid[:np.shape(x_train)[0]]
 ticid_test = ticid[-1 * np.shape(x_test)[0]:]
 
-if supervised:
-    classes = np.loadtxt(fname_class)
-    num_classes = len(np.unique(classes))
-    y_train=np.zeros((np.shape(x_train)[0], num_classes))
-    y_test=np.zeros((np.shape(x_test)[0],num_classes))
-    for i in range(len(classes)):
-        if i < np.shape(x_train)[0]:
-            y_train[i][int(classes[i])] = 1.
-        else:
-            y_test[i-np.shape(x_train)[0]][int(classes[i])] = 1.
-    # y_train = classes[:np.shape(x_train)[0]]
-    # y_test = classes[np.shape(x_train)[0]:]
-    # y_train = np.reshape(y_train, (np.shape(y_train)[0], 1))
-    # y_test = np.reshape(y_test, (np.shape(y_test)[0], 1))
+# if supervised:
+#     num_classes = len(np.unique(classes))
+
+#     y_train=np.zeros((np.shape(x_train)[0], num_classes))
+#     y_test=np.zeros((np.shape(x_test)[0],num_classes))
+#     for i in range(len(classes)):
+#         if i < np.shape(x_train)[0]:
+#             y_train[i][int(classes[i])] = 1.
+#         else:
+#             y_test[i-np.shape(x_train)[0]][int(classes[i])] = 1.
     
 if dual_input:
     rms_train = ml.rms(x_train)
     rms_test = ml.rms(x_test)
+else: rms_train, rms_test = [False, False]
 
 # x_train = np.delete(x_train, 5606, axis = 0) # buggy
 # x_train = np.delete(x_train, 2688, axis = 0)
@@ -173,27 +182,38 @@ for i in range(len(p_list)):
     if supervised:
         history, model = ml.autoencoder(ml.standardize(x_train),
                                         ml.standardize(x_test), p,
-                                        supervised=True,
+                                        dual_input=dual_input,
+                                        rms_train=rms_train, rms_test=rms_test,
+                                        supervised=supervised,
                                         y_train=y_train, y_test=y_test,
                                         num_classes=num_classes)
-        x_predict = model.predict(ml.standardize(x_test))
+        if dual_input:
+            x_predict = model.predict([ml.standardize(x_test), rms_test])
+        else:
+            x_predict = model.predict(ml.standardize(x_test))
         print('true:')
         print(y_test)
         print('predicted:')
         print(x_predict)
-    elif dual_input:
-        history, model = \
-            ml.autoencoder_dual_input2(ml.standardize(x_train),
-                                      ml.standardize(x_test),
-                                      rms_train, rms_test, p)
-        x_predict = model.predict([ml.standardize(x_test), rms_test])
+    # elif dual_input:
+        # history, model = \
+        #     ml.autoencoder_dual_input2(ml.standardize(x_train),
+        #                               ml.standardize(x_test),
+        #                               rms_train, rms_test, p)
+        # x_predict = model.predict([ml.standardize(x_test), rms_test])
     # history, model = ml.autoencoder21(x_train, x_test, p)
     else:
         history, model = ml.autoencoder(ml.standardize(x_train),
                                         ml.standardize(x_test), p,
-                                        supervised=False)
-        
-        x_predict = model.predict(ml.standardize(x_test))
+                                        dual_input=dual_input,
+                                        rms_train=rms_train, rms_test=rms_test,
+                                        supervised=supervised,
+                                        y_train=y_train, y_test=y_test,
+                                        num_classes=num_classes)
+        if dual_input:
+            x_predict = model.predict([ml.standardize(x_test), rms_test])
+        else:
+            x_predict = model.predict(ml.standardize(x_test))
 
     # -- param summary txt ----------------------------------------------------
         
@@ -206,15 +226,12 @@ for i in range(len(p_list)):
         for j in range(4):
             f.write(label_list[j]+' '+str(history.history[key_list[j]][-1])+\
                     '\n')
-        if supervised:
-            chi_2 = np.average(np.sum((x_predict - y_test)**2 / (y_test),
-                                      axis = 1))
-        else:
+        if not supervised:
             # >> assuming uncertainty of 0.02
             chi_2 = np.average((x_predict-ml.standardize(x_test))**2 / 0.02)
             # chi_2 = np.average(np.sum((x_predict - x_test)**2 / (x_test),
             #                           axis = 1))
-        f.write('chi_squared ' + str(chi_2) + '\n')
+            f.write('chi_squared ' + str(chi_2) + '\n')
         if supervised:
             # >> confusion matrix
             # y_true = np.argmax(x_test, axis = 1)
@@ -225,17 +242,14 @@ for i in range(len(p_list)):
             # cm = confusion_matrix(y_test, y_pred, labels=[0.,1.])
             
             y_predict = np.argmax(x_predict, axis=-1)
-            y_test_classes = classes[np.shape(x_train)[0]:]
-            cm = confusion_matrix(np.round(y_predict), y_test_classes)
+            y_true = np.argmax(y_test, axis=-1)
+            cm = confusion_matrix(np.round(y_predict), y_true)
             f.write('confusion matrix\n')
             f.write(str(cm))
             f.write('\ny_true\n')
-            f.write(str(y_test_classes.astype('int'))+'\n')
+            f.write(str(y_true)+'\n')
             f.write('y_predict\n')
             f.write(str(y_predict)+'\n')
-            # f.write('    0   1\n')
-            # f.write('0 ' + str(cm[0]) + '\n')
-            # f.write('1 ' + str(cm[1]) + '\n')
         f.write('\n')
 
 
@@ -253,11 +267,8 @@ for i in range(len(p_list)):
                                          str(i)+'.png', addend=addend,
                                          sharey=False)
     # >> plot latent space
-    if dual_input:
-        activations = ml.get_activations(model, ml.standardize(x_test),
-                                         rms_test = rms_test, dual_input=True)
-    else:
-        activations = ml.get_activations(model, ml.standardize(x_test))
+    activations = ml.get_activations(model, ml.standardize(x_test),
+                                     rms_test = rms_test, dual_input=True)
     if plot_latent_test:
         fig, axes = ml.latent_space_plot(model, activations, p,
                                          output_dir+'latent_space-'+str(i)+\
@@ -265,9 +276,10 @@ for i in range(len(p_list)):
     if plot_latent_train:
         fig, axes = ml.latent_space_plot(model,
                                          ml.get_activations(model,
-                                                            ml.standardize(x_train)),
-                                         p,
-                                         output_dir+'latent_space-x_train-'+\
+                                                            ml.standardize(x_train),
+                                                            rms_test=rms_train,
+                                                            dual_input=dual_input),
+                                         p,output_dir+'latent_space-x_train-'+\
                                              str(i)+'.png')
 
     # >> plot kernel vs. filter
@@ -302,60 +314,11 @@ for i in range(len(p_list)):
                                    ticid_test, out=output_dir+\
                                        'clustering-x_test-'+str(i)+'-',
                                    addend=addend)
+            
+    if supervised:
+        y_train_classes = classes[:np.shape(x_train)[0]]
+        ml.training_test_plot(x,ml.normalize(x_train),ml.normalize(x_test),
+                              y_train_classes,y_true,y_predict,num_classes,
+                              out=output_dir+'lc-'+str(i)+'-')
 
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-# # >> parameters
-# p = {'kernel_size': [21],
-#      'latent_dim': [1, 3],
-#      'strides': [1],
-#      'epochs': [4],
-#      'dropout': [0.1],
-#      'num_conv_layers': [3],
-#      'num_filters': [[16, 32, 32]],
-#      'batch_size': [256],
-#      'activation': ['relu'],
-#      'optimizer': ['adadelta'],
-#      'last_activation': ['sigmoid'],
-#      'losses': ['mean_squared_error', 'binary_crossentropy']}
-
-# p = {'kernel_size': [21],
-#      'latent_dim': [1,2,3,4],
-#      'strides': [1],
-#      'epochs': [20],
-#      'dropout': [0.1],
-#      'num_conv_layers': [3],
-#      'num_filters': [[16, 32, 32]],
-#      'batch_size': [256],
-#      'activation': ['relu'],
-#      'optimizer': ['adadelta'],
-#      'last_activation': ['sigmoid'],
-#      'losses': ['mean_squared_error']}
-
-# >> parameters
-# p = {'kernel_size': 21,
-#      'latent_dim': 3,
-#      'strides': 1,
-#      'epochs': 4,
-#      'dropout': 0.5,
-#      'num_conv_layers': 3,
-#      'num_filters': [32, 32, 32],
-#      'batch_size': 128,
-#      'activation': 'relu',
-#      'optimizer': 'adadelta',
-#      'last_activation': 'sigmoid',
-#      'losses': 'mean_squared_loss'}
-
-# p = {'kernel_size': np.arange(5, 501, 2),
-#      'latent_dim': [25],
-#      'strides' : [1],
-#      'epochs': [2],
-#      'dropout': np.arange(0, 1., 0.05),
-#      'num_conv_layers': [3,5,7,9,11,13],
-#      'num_filters': [8,16,32,64, 128],
-#      'batch_size': [32,64,128,256],
-#      'activation': ['relu'],
-#      'optimizer': ['adadelta', 'sgd', 'adam'],
-#      'last_activation': ['relu', 'sigmoid'],
-#      'losses': ['mean_squared_error', 'binary_crossentropy'],
-#      'noise': [0.]}
