c777386a2a4344ab047d35470fd58d33c164d3c6
emmachickles <53268378+emmachickles@users.noreply.github.com>
2020-04-30 13:19:25 -1000

    sigmaclips lc

--- emma/readtessdata.py
+++ emma/readtessdata.py
@@ -1,10 +1,10 @@
-# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 #
 # read tess data (sector 20)
 # time in TESS-truncated JD (BJD - 2457000)
-# emma 03/16/2020
+# emma 0420
 #
-# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 from astropy.io import fits
 import numpy as np
@@ -12,41 +12,54 @@ import pdb
 import os
 import fnmatch
 import matplotlib.pyplot as plt
-from itertools import groupby
+from astropy.stats import SigmaClip
 
-# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 fitspath = './tessdata_lc/'
-output_dir = './lightcurves031820/'
+# fitspath = './tessdata_sector20/'
+output_dir = './plots/lightcurves032920/'
 
-# fnames_all = os.listdir(fitspath)
-# fnames = fnmatch.filter(fnames_all, '*fits*')
+fnames_all = os.listdir(fitspath)
+fnames = fnmatch.filter(fnames_all, '*fits*')
 # # >> remove buggy fits file (interrupted download)
-# fnames.pop(fnames.index('tess2019357164649-s0020-0000000156168236-0165-s_lc.fits'))
+fnames.pop(fnames.index('tess2019357164649-s0020-0000000156168236-0165-s_lc.fits'))
 
-fnames = ['tess2019357164649-s0020-0000000004287518-0165-s_lc.fits',
-          'tess2019357164649-s0020-0000000004305219-0165-s_lc.fits',
-          'tess2019357164649-s0020-0000000009264019-0165-s_lc.fits',
-          'tess2019357164649-s0020-0000000011673333-0165-s_lc.fits',
-          'tess2019357164649-s0020-0000000015863853-0165-s_lc.fits',
-          'tess2019357164649-s0020-0000000051238317-0165-s_lc.fits',
-          'tess2019357164649-s0020-0000000099725941-0165-s_lc.fits',
-          'tess2019357164649-s0020-0000000130158361-0165-s_lc.fits',
-          'tess2019357164649-s0020-0000000004375248-0165-s_lc.fits',
-          'tess2019357164649-s0020-0000000081210712-0165-s_lc.fits',
-          'tess2019357164649-s0020-0000000053335431-0165-s_lc.fits',
-          'tess2019357164649-s0020-0000000160160991-0165-s_lc.fits']
-classified = True
-classes = np.array([1., 1., 2., 2., 3., 4., 4., 4., 1., 2., 3., 4.])
+
 
 interp_tol = 20. / (24*60) # >> interpolate small gaps (less than 20 minutes)
 
 intensity = []
-prefix = '12lightcurves-'
+# prefix = 'sector20-'
+# prefix = 'supervised100-'
+prefix = 's0020-1155-'
 
+plot = False
+before_orbit = False
 
+classified = False
+classes = np.array([1., 1., 2., 2., 3., 4., 4., 4., 1., 2., 3., 4.])
+if classified:
+    # with open('./labelled_100.csv', 'r') as f:
+    #     lines = f.readlines()
+    # tics = [line.split()[1][:7] for line in lines[1:]]
+    # classes = [int(line.split()[2]) for line in lines[1:]]
+    with open('./3class-labelled-ticid.txt', 'r') as f:
+        tics = f.readlines()
+        tics = [ticid.split()[0] for ticid in tics]
+    # np.savetxt(prefix + 'classification.txt', classes, delimiter = ',')
+    fnames_classified = []
+    for i in range(len(tics)):
+        fname = list(filter(lambda x: tics[i] in x, fnames))[0]
+        fnames_classified.append(fname)
+    fnames = fnames_classified
+    
+ticids = []
+count = 0
+print(len(fnames))
 for file in fnames:
-    # -- open file -------------------------------------------------------------
+    print(count)
+    # -- open file ------------------------------------------------------------
     f = fits.open(fitspath + file)
     # print(f.info())
     # print(f[1].header)
@@ -54,14 +67,20 @@ for file in fnames:
     # >> get data
     time = f[1].data['TIME']
     i = f[1].data['PDCSAP_FLUX']
+    ticids.append(f[1].header['TICID'])
 
     # >> plot
     # fig, ax = plt.subplots(1,1)
     # ax.plot(time, i, '.')
     # plt.savefig(output_dir + file[14:-5] + '.png')
     # plt.close(fig)
+    
+    # >> sigmaclip
+    sigclip = SigmaClip(sigma=5, maxiters=None, cenfunc='median')
+    clipped_inds = np.nonzero(np.ma.getmask(sigclip(i, masked=True)))
+    i[clipped_inds] = np.nan
 
-    # -- find small nan gaps ---------------------------------------------------
+    # -- find small nan gaps --------------------------------------------------
     # >> adapted from https://gist.github.com/alimanfoo/
     #    c5977e87111abe8127453b21204c1065
     # >> find run starts
@@ -80,7 +99,7 @@ for file in fnames:
     interp_lens = run_lengths[np.nonzero((run_lengths * tdim <= interp_tol) * \
                                          np.isnan(i[run_starts]))]
 
-    # -- interpolation ---------------------------------------------------------
+    # -- interpolation --------------------------------------------------------
     # >> interpolate small gaps
     i_interp = np.copy(i)
     for a in range(np.shape(interp_inds)[0]):
@@ -91,23 +110,49 @@ for file in fnames:
                                                 i[np.nonzero(~np.isnan(i))])
     intensity.append(i_interp)
 
-# -- remove orbit nan gap ------------------------------------------------------
+    count += 1
+
+# -- remove orbit nan gap -----------------------------------------------------
 intensity = np.array(intensity)
-# nan_inds = np.nonzero(np.prod(np.isnan(intensity)==False), axis = 0))
 nan_inds = np.nonzero(np.prod(np.isnan(intensity)==False, axis = 0) == False)
+
+if before_orbit:
+    orbit_gap_start = np.argmax(np.diff(time))
+    intensity = intensity[:,:orbit_gap_start]
+    time = time[:orbit_gap_start]
+
 time = np.delete(time, nan_inds)
 intensity = np.delete(intensity, nan_inds, 1)
 
-# >> save as txt file
-# np.savetxt('tessdatasector20-time.txt', time, delimiter = ',')
-# np.savetxt('tessdatasector20-intensity.csv', intensity, delimiter=',', fmt='%d')
-np.savetxt(prefix + 'time.txt', time, delimiter = ',')
-np.savetxt(prefix + 'intensity.csv', intensity, delimiter = ',', fmt = '%d')
-np.savetxt(prefix + 'classification.txt', classes, delimiter = ',')
+# -- save as txt file ---------------------------------------------------------
 
-# -- plot ----------------------------------------------------------------------
-# for i in range(np.shape(intensity)[0]):
-    # fig, ax = plt.subplots(1,1)
-    # ax.plot(time, intensity[i], '.')
-    # plt.savefig(output_dir + fnames[i][14:-5] + 'interpolated' + '.png')
-    # plt.close(fig)
+np.savetxt(prefix + 'time.txt', time, delimiter = ',')
+np.savetxt(prefix + 'flux.csv', intensity, delimiter = ',', fmt = '%d')
+np.savetxt(prefix + 'ticid.txt', ticids, delimiter=',')
+
+if classified:
+    np.savetxt(prefix + 'classification.txt', classes, delimiter = ',')
+
+# -- plot ---------------------------------------------------------------------
+if plot:
+    for i in range(np.shape(intensity)[0]):
+        fig, ax = plt.subplots(1,1)
+        ax.plot(time, intensity[i], '.')
+        plt.savefig(output_dir + fnames[i][14:-5] + 'interpolated' + '.png')
+        plt.close(fig)
+    
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    
+    
+# fnames = ['tess2019357164649-s0020-0000000004287518-0165-s_lc.fits',
+#           'tess2019357164649-s0020-0000000004305219-0165-s_lc.fits',
+#           'tess2019357164649-s0020-0000000009264019-0165-s_lc.fits',
+#           'tess2019357164649-s0020-0000000011673333-0165-s_lc.fits',
+#           'tess2019357164649-s0020-0000000015863853-0165-s_lc.fits',
+#           'tess2019357164649-s0020-0000000051238317-0165-s_lc.fits',
+#           'tess2019357164649-s0020-0000000099725941-0165-s_lc.fits',
+#           'tess2019357164649-s0020-0000000130158361-0165-s_lc.fits',
+#           'tess2019357164649-s0020-0000000004375248-0165-s_lc.fits',
+#           'tess2019357164649-s0020-0000000081210712-0165-s_lc.fits',
+#           'tess2019357164649-s0020-0000000053335431-0165-s_lc.fits',
+#           'tess2019357164649-s0020-0000000160160991-0165-s_lc.fits']
