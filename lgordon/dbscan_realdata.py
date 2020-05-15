b29870e8906dcb9973950f724c7e06f7bfdf80bb
Lindsey Gordon <53380890+lcgordon@users.noreply.github.com>
2020-05-15 13:31:05 -0400

    so i don't lose this if things go awry

--- lgordon/dbscan_realdata.py
+++ lgordon/dbscan_realdata.py
@@ -128,26 +128,112 @@ for i in range(len(outliers)):
 featvec_reduced = np.delete(lc_feat, outliers, 0)   
 print(len(featvec_reduced), len(lc_feat))
 
-
-
-
-
-
 #%%
+def plot_all_insets(feature_vectors,targets, intensity, time, feat1, feat2, graph_label1, graph_label2):
+    """plots the x/y min/max points' associated light curve on the plot"""
+    fig, ax1 = plt.subplots()
+    ax1.scatter(feature_vectors[:,feat1], feature_vectors[:,feat2], c = "black")
+    ax1.set_xlabel(graph_label1)
+    ax1.set_ylabel(graph_label2)
+    
+    plot_inset(ax1, "axins1", targets, intensity, time,feature_vectors, feat1, feat2)
+
+    
+def plot_inset(ax1, axis_name, targets, intensity, time, feature_vectors, feat1, feat2):
+    """ plots the inset plots. 
+    ax1 is the name of the axis being used. it is ESSENTIAL to getting this to run
+    axis_name should be axins + a number as a STRING
+    feat1 is x axis, feat2 is yaxis
+    extreme is which point you want to plot- options are 'ymax', 'ymin'
+    if no extreme is given, prints that you fucked up."""
+    range_x = feature_vectors[:,feat1].max() - feature_vectors[:,feat1].min()
+    range_y = feature_vectors[:,feat2].max() - feature_vectors[:,feat2].min()
+    x_offset = range_x * 0.001
+    y_offset = range_y * 0.001
+    
+    indexes_unique, targets_to_plot, tuples_plotting, titles = get_extrema(feature_vectors, targets, feat1, feat2)
+    #print(indexes_unique)
+    for n in range(len(indexes_unique)):
+        x_shift = np.random.randint(0,2)
+        y_shift = np.random.randint(0,2)
+        index = indexes_unique[n]
+        thetuple = tuples_plotting[n]
+        title = titles[n]
         
-def check_box_location(coordtuple, feat1, feat2, range_x, range_y, x, y):
-    """ checks if data points lie within the area of the inset plot"""
+        inset_x, inset_y, inset_width, inset_height = check_box_location(feature_vectors, thetuple, feat1, feat2, range_x, range_y, x_shift, y_shift)
+        #inset_width, inset_height, inset_x, inset_y = box_locate_no_repositioning(thetuple, range_x, range_y, x_shift, y_shift)
+        
+        axis_name = ax1.inset_axes([inset_x, inset_y, inset_width, inset_height], transform = ax1.transData) #x pos, y pos, width, height
+        axis_name.scatter(time, intensity[index], c='black', s = 0.01)
+            
+        x1, x2, y1, y2 =  feature_vectors[index][feat1], feature_vectors[index][feat1] + x_offset, feature_vectors[index][feat2], feature_vectors[index][feat2] + y_offset
+        axis_name.set_xlim(x1, x2)
+        axis_name.set_ylim(y1, y2)
+        ax1.indicate_inset_zoom(axis_name)
+            
+        inset_labelling(axis_name, time, intensity, targets, index, title)
+
+        
+
+def get_extrema(feature_vectors, targets, feat1, feat2):
+    indexes = []
+    index_feat1 = np.argsort(feature_vectors[:,feat1])
+    index_feat2 = np.argsort(feature_vectors[:,feat2])
+    indexes.append(index_feat1[-1]) #largest
+    indexes.append(index_feat1[-2]) #second largest
+    indexes.append(index_feat1[0]) #smallest
+    indexes.append(index_feat1[1]) #second smallest
+
+    indexes.append(index_feat2[-1]) #largest
+    indexes.append(index_feat2[-2]) #second largest
+    indexes.append(index_feat2[0]) #smallest
+    indexes.append(index_feat2[1]) #second smallest
+
+    indexes_unique = np.unique(np.asarray(indexes))
+    
+    targets_to_plot = []
+    tuples_plotting = []
+    titles = []
+    
+    for n in range(len(indexes_unique)):
+        targets_to_plot.append(targets[indexes_unique[n]])
+        tuples_plotting.append( (feature_vectors[:,feat1][indexes_unique[n]], feature_vectors[:,feat2][indexes_unique[n]]) )
+        title = astroquery_pull_data(targets[indexes_unique[n]])
+        titles.append(title)
+    return indexes_unique, targets_to_plot, tuples_plotting, titles
+
+def box_locate_no_repositioning(coordtuple, range_x, range_y, x_shift, y_shift):
+    inset_width = range_x / 3
+    inset_height = range_y / 8
+    if x_shift == 0:
+        inset_x = coordtuple[0] - (inset_width * 4) #move left
+    elif x_shift == 1:
+        inset_x = coordtuple[0] + (inset_width * 4) #move right
+    if y_shift == 0:
+        inset_y = coordtuple[1] + (inset_height) #move up
+    elif y_shift == 1:
+        inset_y = coordtuple[1] - (inset_height) #move down
+    
+    return inset_width, inset_height, inset_x, inset_y
+
+def check_box_location(feature_vectors, coordtuple, feat1, feat2, range_x, range_y, x, y):
+    """ checks if data points lie within the area of the inset plot
+    coordtuple is the (x,y) point in feature space
+    feat1, feat2 are the number for the features being used
+    range_x and range_y are ranges for each feature
+    x is whether it will be left or right of the point
+    y is whether it will be above/below the point"""
     #position of box - needs to be dependent on location
-    inset_width = range_x / 2
-    inset_height = range_y /5 
-    if x == 'left':
-        inset_x = coordtuple[0] - inset_width * 1.2
-    elif x == 'right':
-        inset_x = coordtuple[0] + 0.01 * range_x
-    if y == 'up':
-        inset_y = coordtuple[1] + 0.01 * range_y
-    elif y == 'down':
-        inset_y = coordtuple[1] - inset_height * 1.2
+    inset_width = range_x / 3
+    inset_height = range_y /8
+    if x == 0:
+        inset_x = coordtuple[0] - (inset_width * 1.2) #move left
+    elif x == 1:
+        inset_x = coordtuple[0] + (inset_width * 1.2) #move right
+    if y == 0:
+        inset_y = coordtuple[1] + (inset_height) #move up
+    elif y == 1:
+        inset_y = coordtuple[1] - (inset_height) #move down
     
     
     inset_BL = (inset_x, inset_y)
@@ -155,180 +241,40 @@ def check_box_location(coordtuple, feat1, feat2, range_x, range_y, x, y):
     inset_TL = (inset_x, inset_y + inset_height)
     inset_TR = (inset_x + inset_width, inset_y + inset_height)
     
-    conc = np.column_stack((lc_feat[:,feat1], lc_feat[:,feat2]))
+    conc = np.column_stack((feature_vectors[:,feat1], feature_vectors[:,feat2]))
     polygon = Polygon([inset_BL, inset_BR, inset_TL, inset_TR])
     
     i = 0
     n = len(conc)
     
+    inset_positions = []
+    
     while i < n:
         point = Point(conc[i])
         if polygon.contains(point) == True:
-            inset_x += 0.001 * range_x
-            inset_y += 0.001 * range_y
+            if x == 0: 
+                inset_x = inset_x - (0.01 * range_x)
+            elif x == 1:
+                inset_x = inset_x + (0.01 * range_x)
+            if y == 0:
+                inset_y = inset_y + (0.01 * range_y)
+            elif y == 1:
+                inset_y = inset_y - (0.01 * range_y)
+            
+            inset_BL = (inset_x, inset_y)
+            inset_BR = (inset_x + inset_width, inset_y)
+            inset_TL = (inset_x, inset_y + inset_height)
+            inset_TR = (inset_x + inset_width, inset_y + inset_height)
+            polygon = Polygon([inset_BL, inset_BR, inset_TL, inset_TR])
             i = 0
-            print("moving")
+            #print("moving")
         elif polygon.contains(point) == False:
             i = i + 1
+    print("position determined")
     return inset_x, inset_y, inset_width, inset_height
 
-def astroquery_pull_data(target):
-    """pulls data on object from astroquery
-    target needs to be a string"""
-    try: 
-        catalog_data = Catalogs.query_object(target, radius=0.02, catalog="TIC")
-        #https://arxiv.org/pdf/1905.10694.pdf
-        T_eff = catalog_data[0]["Teff"]
-        obj_type = catalog_data[0]["objType"]
-        gaia_mag = catalog_data[0]["GAIAmag"]
-        radius = catalog_data[0]["rad"]
-        mass = catalog_data[0]["mass"]
-        distance = catalog_data[0]["d"]
-        title = "\nT_eff:" + str(T_eff) + ", ObjType: " + str(obj_type) + ", GAIA mag: " + str(gaia_mag) + "\n Dist: " + str(distance) + ", Radius:" + str(radius) + " Mass:" + str(mass)
-    except (ConnectionError, OSError):
-        print("there was a connection error!")
-        title = "connection error, no data"
-    return title
-
-def inset_labelling(axis_name, index, title):
-    axis_name.set_xlim(time[0], time[-1])
-    axis_name.set_ylim(intensity[index].min(), intensity[index].max())
-    axis_name.set_xlabel("BJD [2457000]")
-    axis_name.set_ylabel("relative flux")
-    axis_name.set_title(targets[index] + title, fontsize=8)
-
 #%%
-def max_y(feat1, feat2, range_x, range_y):
-    """produce maximum y information"""
-    y_max_index = np.argmax(lc_feat[:,feat2])
-    targ_y_max = targets[y_max_index]
-    y_max_tuple = (lc_feat[:,feat1][y_max_index], lc_feat[:,feat2][y_max_index])
-    inset_x, inset_y, inset_width, inset_height = check_box_location(y_max_tuple, feat1, feat2, range_x, range_y, "left", 'up')
-    title = astroquery_pull_data(targ_y_max)
-    return y_max_index, title, inset_x, inset_y, inset_width, inset_height
-
-def min_y(feat1, feat2, range_x, range_y):
-    """produce minimum y information"""
-    y_min_index = np.argmin(lc_feat[:,feat2])
-    targ_y_min = targets[y_min_index]
-    y_min_tuple = (lc_feat[:,feat1][y_min_index], lc_feat[:,feat2][y_min_index])
-    inset_x, inset_y, inset_width, inset_height = check_box_location(y_min_tuple, feat1, feat2, range_x, range_y, "left", "down")
-    title = astroquery_pull_data(targ_y_min)
-    return y_min_index, title, inset_x, inset_y, inset_width, inset_height
-
-def max_x(feat1, feat2, range_x, range_y):
-    """maximum x point inset"""
-    x_max_index = np.argmax(lc_feat[:,feat1])
-    targ_x_max = targets[x_max_index]
-    x_max_tuple = (lc_feat[:,feat1][x_max_index], lc_feat[:,feat2][x_max_index])
-    inset_x, inset_y, inset_width, inset_height = check_box_location(x_max_tuple, feat1, feat2, range_x, range_y, 'right', 'down')
-    title = astroquery_pull_data(targ_x_max)
-    return x_max_index, title, inset_x, inset_y, inset_width, inset_height
-    
-def min_x(feat1, feat2, range_x, range_y):
-    """produce minimum y information"""
-    x_min_index = np.argmin(lc_feat[:,feat1])
-    targ_x_min = targets[x_min_index]
-    y_min_tuple = (lc_feat[:,feat1][x_min_index], lc_feat[:,feat2][x_min_index])
-    inset_x, inset_y, inset_width, inset_height = check_box_location(y_min_tuple, feat1, feat2, range_x, range_y, "left", "up")
-    title = astroquery_pull_data(targ_x_min)
-    return x_min_index, title, inset_x, inset_y, inset_width, inset_height
-    
-    
-def plot_inset(ax1, axis_name, feat1, feat2, extreme, index_list):
-    """ plots the inset plots. axis_name should be axins + a number as a STRING
-    feat1 is x axis, feat2 is yaxis
-    extreme is which point you want to plot- options are 'ymax', 'ymin'
-    if no extreme is given, prints that you fucked up."""
-    range_x = lc_feat[:,feat1].max() - lc_feat[:,feat1].min()
-    range_y = lc_feat[:,feat2].max() - lc_feat[:,feat2].min()
-    x_offset = range_x * 0.001
-    y_offset = range_y * 0.001
-    if extreme == "ymax":
-        index, title, inset_x, inset_y, inset_width, inset_height = max_y(feat1,feat2, range_x, range_y)
-        #print(index, title, inset_x, inset_y, inset_width, inset_height)
-        if index in index_list: 
-            print("this has already been plotted")
-        else: 
-            index_list.append(index)
-            axis_name = ax1.inset_axes([inset_x, inset_y, inset_width, inset_height], transform = ax1.transData) #x pos, y pos, width, height
-            axis_name.scatter(time, intensity[index], c='black', s = 0.01)
-            
-            x1, x2, y1, y2 =  lc_feat[index][feat1], lc_feat[index][feat1] + x_offset, lc_feat[index][feat2], lc_feat[index][feat2] + y_offset
-            axis_name.set_xlim(x1, x2)
-            axis_name.set_ylim(y1, y2)
-            ax1.indicate_inset_zoom(axis_name)
-            
-            inset_labelling(axis_name, index, title)
-    elif extreme == 'ymin':
-        index, title, inset_x, inset_y, inset_width, inset_height = min_y(feat1,feat2, range_x, range_y)
-        if index in index_list: 
-            print("this has already been plotted")
-        else:
-            index_list.append(index)
-            axis_name = ax1.inset_axes([inset_x, inset_y, inset_width, inset_height], transform = ax1.transData) #x pos, y pos, width, height
-            axis_name.scatter(time, intensity[index], c='black', s = 0.01)
-            
-            x1, x2, y1, y2 =  lc_feat[index][feat1], lc_feat[index][feat1] + x_offset, lc_feat[index][feat2], lc_feat[index][feat2] + y_offset
-            axis_name.set_xlim(x1, x2)
-            axis_name.set_ylim(y1, y2)
-            ax1.indicate_inset_zoom(axis_name)
-            
-            inset_labelling(axis_name, index, title)
-    elif extreme == 'xmax':
-        index, title, inset_x, inset_y, inset_width, inset_height = max_x(feat1,feat2, range_x, range_y)
-        if index in index_list: 
-            print("this has already been plotted")
-        else:
-            index_list.append(index)
-            axis_name = ax1.inset_axes([inset_x, inset_y, inset_width, inset_height], transform = ax1.transData) #x pos, y pos, width, height
-            axis_name.scatter(time, intensity[index], c='black', s = 0.01)
-            
-            x1, x2, y1, y2 =  lc_feat[index][feat1], lc_feat[index][feat1] + x_offset, lc_feat[index][feat2], lc_feat[index][feat2] + y_offset
-            axis_name.set_xlim(x1, x2)
-            axis_name.set_ylim(y1, y2)
-            ax1.indicate_inset_zoom(axis_name)
-            
-            inset_labelling(axis_name, index, title)
-    elif extreme == 'xmin':
-        index, title, inset_x, inset_y, inset_width, inset_height = min_x(feat1,feat2, range_x, range_y)
-        if index in index_list: 
-            print("this has already been plotted")
-        else:
-            index_list.append(index)
-            axis_name = ax1.inset_axes([inset_x, inset_y, inset_width, inset_height], transform = ax1.transData) #x pos, y pos, width, height
-            axis_name.scatter(time, intensity[index], c='black', s = 0.01)
-            
-            x1, x2, y1, y2 =  lc_feat[index][feat1], lc_feat[index][feat1] + x_offset, lc_feat[index][feat2], lc_feat[index][feat2] + y_offset
-            axis_name.set_xlim(x1, x2)
-            axis_name.set_ylim(y1, y2)
-            ax1.indicate_inset_zoom(axis_name)
-            
-            inset_labelling(axis_name, index, title)
-    else: 
-        print(axis_name + ": this extreme does not exist and cannot be plotted")
-#%%
-
-def plot_4_insets(feat1, feat2, graph_label1, graph_label2):
-        
-    fig, ax1 = plt.subplots()
-    ax1.scatter(lc_feat[:,feat1], lc_feat[:,feat2], c = "black")
-    ax1.set_xlabel(graph_label1)
-    ax1.set_ylabel(graph_label2)
-    
-    index_list = []
-    
-    plot_inset(ax1, "axins1", feat1, feat2, 'ymax', index_list)
-    plot_inset(ax1, "axins2", feat1, feat2, 'ymin', index_list)
-    plot_inset(ax1, "axins3", feat1, feat2, 'xmax', index_list)
-    plot_inset(ax1,"axins4", feat1, feat2, 'xmin', index_list)
-    plt.tight_layout()
-
-    plt.savefig("/Users/conta/UROP_Spring_2020/plot_output/5-13/inset-plot-15.png")
-    
-#%%
-#make this bad boy a function
-def n_choose_2_insets(feature_vectors, date):
+def n_choose_2_insets(time, intensity, feature_vectors, targets, date):
     """plotting (n 2) features against each other w/ 4 extremes inset plotted
     feature_vectors is the list of ALL feature_vectors
     date must be a string in the format of the folder you are saving into ie "4-13"
@@ -339,7 +285,8 @@ def n_choose_2_insets(feature_vectors, date):
     except OSError:
         print ("Creation of the directory %s failed" % path)
         print("New folder created will have -new at the end. Please rename.")
-        os.makedirs(path + "-new")
+        path = path + "-new"
+        os.makedirs(path)
     else:
         print ("Successfully created the directory %s" % path) 
  
@@ -350,24 +297,20 @@ def n_choose_2_insets(feature_vectors, date):
     fname_labels = ["Avg", "Var", "Skew", "Kurt", "LogVar", "LogSkew", "LogKurt",
                     "MaxPower", "LogMaxPower", "Period0_1to10", "Slope", "LogSlope",
                     "P0", "P1", "P2", "Period0to0_1"]
-    for n in range(8):
+    for n in range(3):
         #feat1 = feature_vectors[:,n]
         graph_label1 = graph_labels[n]
         fname_label1 = fname_labels[n]
-        for m in range(8):
+        for m in range(3):
             if m == n:
                 continue
             graph_label2 = graph_labels[m]
             fname_label2 = fname_labels[m]                
             #feat2 = feature_vectors[:,m]
             
-            plot_4_insets(n, m, graph_label1, graph_label2)
-            #plt.scatter(feat1, feat2, s = 2, color = 'black')
-            #plt.xlabel(graph_label1)
-            #plt.ylabel(graph_label2)
+            plot_all_insets(feature_vectors, targets, intensity, time, n, m, graph_label1, graph_label2)
  
             plt.savefig("/Users/conta/UROP_Spring_2020/plot_output/" + date + "/nchoose2/" + date + "-" + fname_label1 + "-vs-" + fname_label2 + ".png")
             plt.show()
 
-#%%
-n_choose_2_insets(lc_feat, "5-13")
+
